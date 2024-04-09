129456d15c4047d508c9295bc99880daf7906e8d
Smirnov Alex <aasmirnov@tsum.ru>
2024-04-09 14:24:07 +0300

    Подключает АПИ для получения списка предложений

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './app';
+import { TPreviewOffer } from './types/preview-offer';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const api = async (): Promise<TPreviewOffer[]> => {
+  const response = await fetch(
+    'https://15.design.htmlacademy.pro/six-cities/offers'
+  ).then((res) => res.json() as Promise<TPreviewOffer[]>);
+
+  return response;
+};
+
+const offers: TPreviewOffer[] = await api();
+
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App offers={offers} />
   </React.StrictMode>
 );
