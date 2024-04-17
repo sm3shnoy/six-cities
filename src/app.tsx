007b7045c7213745b2e0e6f45ae732e8941f6d2a
Smirnov Alex <aasmirnov@tsum.ru>
2024-04-17 10:47:14 +0300

    Добавил роутинг

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,40 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/main-page';
 import { TPreviewOffer } from './types/preview-offer';
+import { AppRoutes } from './const';
+import NotFoundPage from './pages/not-found-page';
+import { FavoritesPage } from './pages/favorites-page';
+import { Login } from './pages/login-page';
+import { OfferPage } from './pages/offer-page';
+import { Layout } from './components/layout';
+import { PrivateRoute } from './components/private-route/private-route';
 
 const App = ({ offers }: { offers: TPreviewOffer[] }) => (
-  <MainPage offers={offers} />
+  <BrowserRouter>
+    <Routes>
+      <Route path={AppRoutes.Main} element={<Layout />}>
+        <Route index element={<MainPage offers={offers} />} />
+        <Route
+          path={AppRoutes.Favorites}
+          element={
+            <PrivateRoute>
+              <FavoritesPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoutes.Login}
+          element={
+            <PrivateRoute isOnlyUnAuth>
+              <Login />
+            </PrivateRoute>
+          }
+        />
+        <Route path={AppRoutes.OfferId} element={<OfferPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
