9d61797ec129a1e1b7158cdef8a6ba0ecac2872d
Smirnov Alex <aasmirnov@tsum.ru>
2024-07-24 17:54:39 +0300

    fix: рефакторинг получения токена

--- src/services/token.ts
+++ src/services/token.ts
@@ -2,11 +2,8 @@ export const AUTH_TOKEN_KEY_NAME = 'six-cities-token';
 
 export type Token = string;
 
-export const getToken = (): Token => {
-  const token = localStorage.getItem(AUTH_TOKEN_KEY_NAME);
-
-  return token ?? '';
-};
+export const getToken = (): Token =>
+  localStorage.getItem(AUTH_TOKEN_KEY_NAME) ?? '';
 
 export const setToken = (token: Token) =>
   localStorage.setItem(AUTH_TOKEN_KEY_NAME, token);
