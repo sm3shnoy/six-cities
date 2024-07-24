b8606559d3ae4dcd7a8f3dd53bee1956957441c1
Smirnov Alex <aasmirnov@tsum.ru>
2024-07-24 17:45:08 +0300

    переносит получение оферов и проверку на авторизацию в app

--- src/app.tsx
+++ src/app.tsx
@@ -1,39 +1,66 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/main-page';
-import { AppRoutes } from './const';
+import { AppRoutes, RequestStatus } from './const';
 import NotFoundPage from './pages/not-found-page';
 import { FavoritesPage } from './pages/favorites-page';
 import { Login } from './pages/login-page';
 import { OfferPage } from './pages/offer-page';
 import { Layout } from './components/layout';
 import { PrivateRoute } from './components/private-route/private-route';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from './store/hooks';
+import { checkAuthAction } from './store/thunks/user';
+import { getToken } from './services/token';
+import { offersActions } from './store/slices/offers';
+import { userSelectors } from './store/slices/user';
+import { Spinner } from './components/spinner';
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path={AppRoutes.Main} element={<Layout />}>
-        <Route index element={<MainPage />} />
-        <Route
-          path={AppRoutes.Favorites}
-          element={
-            <PrivateRoute>
-              <FavoritesPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path={AppRoutes.Login}
-          element={
-            <PrivateRoute isOnlyUnAuth>
-              <Login />
-            </PrivateRoute>
-          }
-        />
-        <Route path={AppRoutes.OfferId} element={<OfferPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-);
+const App = () => {
+  const dispatch = useAppDispatch();
+  const authLoadingStatus = useAppSelector(userSelectors.requestStatus);
+  const token = getToken();
+
+  useEffect(() => {
+    dispatch(offersActions.fetchOffersAction());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(checkAuthAction());
+    }
+  }, [dispatch, token]);
+
+  if (authLoadingStatus === RequestStatus.Loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoutes.Main} element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route
+            path={AppRoutes.Favorites}
+            element={
+              <PrivateRoute>
+                <FavoritesPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path={AppRoutes.Login}
+            element={
+              <PrivateRoute isOnlyUnAuth>
+                <Login />
+              </PrivateRoute>
+            }
+          />
+          <Route path={AppRoutes.OfferId} element={<OfferPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -6,20 +6,17 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import { fetchOffersAction } from './store/thunks/offers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-store.dispatch(fetchOffersAction());
-
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <HelmetProvider>
+        <ToastContainer position="top-left" />
         <App />
-        <ToastContainer position="bottom-left" />
       </HelmetProvider>
     </Provider>
   </React.StrictMode>
