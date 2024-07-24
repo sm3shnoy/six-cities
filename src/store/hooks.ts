c3be82fe23e5dfb1f45436584d95c684e75ecd2b
Smirnov Alex <aasmirnov@tsum.ru>
2024-07-24 17:54:51 +0300

    добавляет типизацию для useselector

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,10 +1,10 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../types/store';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AxiosInstance } from 'axios';
 
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const createAppAsyncThunk = createAsyncThunk.withTypes<{
   extra: AxiosInstance;
